571a9c7aa28813ac3727fc73250fee0beb11a3d8
Kaiden Reid <362568016@GoTVDSB.ca>
2025-09-16 09:36:59 -0400

    Add project files.

---
+++ Topic 4 Assignment - UserInput/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Topic_4_Assignment___UserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, login;
+            int age, Grade, ID;
+            double salary;
+            
+            Console.WriteLine("Hello, Whats your name?: ");
+            name = Console.ReadLine();
+            Console.WriteLine(" Woah thats a neat name " + name + " How old are you?: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(age + " nice! how much do you make?: ");
+            salary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(salary + " wow thats alot, now please fill out this info :D ");
+            Console.WriteLine("whats your grade?: ");
+            Grade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("whats your Student ID?: ");
+            ID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("whats your Login?: ");
+            login = Console.ReadLine();
+
+            //----------------------------------------
+
+
+        }
+    }
+}
